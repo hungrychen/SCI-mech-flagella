03e58a38672aa7d4b362e0f953736c8f057e0e33
Darren Chin <chindarren@ucla.edu>
2024-05-20 13:37:14 -0700

    Calibration and .gitignore update

--- arduino/loadcellconfig.h
+++ arduino/loadcellconfig.h
@@ -4,8 +4,8 @@
 const bool LC1_REVERSED = true;
 const bool LC2_REVERSED = true;
 
-const float CAL_VAL_1 = 13126.67;         // FRONT
-const float CAL_VAL_2 = 12823.;         // REAR
+const float CAL_VAL_1 = 13079.13;         // FRONT
+const float CAL_VAL_2 = 13201.96;         // REAR
 const unsigned long LC_STABILIZING_DELAY = 5e3;
 
 #endif // LOADCELLCONFIG_H
